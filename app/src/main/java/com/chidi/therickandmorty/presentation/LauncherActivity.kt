0c7ee65000e5f573a69043137c5ad4558789b256
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-06-11 19:04:45 +0100

    (improvement)
    added dependencies to be used to the build.gradle file
    created LauncherActivity.kt to be used as welcome screen

---
+++ app/src/main/java/com/chidi/therickandmorty/presentation/LauncherActivity.kt
@@ -0,0 +1,11 @@
+package com.chidi.therickandmorty.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+    }
+}
