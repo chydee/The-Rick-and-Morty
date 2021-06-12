f5540442991420bf4a2f2f27e50cdcfdf43bc2a5
Ngwuta Desmond <desmondmuqsit@yahoo.com>
2021-06-12 03:07:24 +0100

    (style/improvement)
    created MainApplication.kt: an application class
    updated AndroidManifest.xml
    updated themes.xml: added style attributes for the Toolbar
    created nav_graph.xml
    added AutoClearedValue.kt class
    created DataAccessStrategy.kt to monitor and handle data dispatch
    created Resource class

--- app/src/main/java/com/chidi/therickandmorty/MainActivity.kt
+++ app/src/main/java/com/chidi/therickandmorty/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.chidi.therickandmorty
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.chidi.therickandmorty.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding: ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment: NavHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+
+        val appBarConfiguration: AppBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
